c04d44998a62a2cec5ab4898a78f44cc3d129a79
Engineev <ryw_pub@126.com>
2018-07-29 17:26:41 +0800

    update

--- src/server/external_rpc_service.h
+++ src/server/external_rpc_service.h
@@ -40,37 +40,42 @@ private:
 
 } // namespace ppca
 
-namespace ppca {
+/* Example:
+ *
+ * namespace ppca {
+ *
+ * class ExternalRpcServer {
+ * public:
+ *   template <class Func>
+ *   void bindPut(Func && f) { service.bindPut(std::forward<Func>(f)); }
+ *
+ * template <class Func>
+ * void bindGet(Func && f) { service.bindGet(std::forward<Func>(f)); }
+ *
+ * void Start(std::uint16_t port) {
+ *   std::string addr = "0.0.0.0:" + std::to_string(port);
+ *   grpc::ServerBuilder builder;
+ *   builder.AddListeningPort(addr, grpc::InsecureServerCredentials());
+ *   builder.RegisterService(&service);
+ *   server = builder.BuildAndStart();
+ *   runningThread = std::thread([this] { server->Wait(); });
+ * }
+ *
+ * void Shutdown() {
+ *   if (server)
+ *     server->Shutdown();
+ *   runningThread.join();
+ * }
+ *
+ * private:
+ * ExternalRpcService service;
+ * std::unique_ptr<grpc::Server> server;
+ * std::thread runningThread;
+ * }; // class
+ *
+ * } // namespace ppca
+ */
 
-class ExternalRpcServer {
-public:
-  template <class Func>
-  void bindPut(Func && f) { service.bindPut(std::forward<Func>(f)); }
 
-  template <class Func>
-  void bindGet(Func && f) { service.bindGet(std::forward<Func>(f)); }
-
-  void Start(std::uint16_t port) {
-    std::string addr = "0.0.0.0:" + std::to_string(port);
-    grpc::ServerBuilder builder;
-    builder.AddListeningPort(addr, grpc::InsecureServerCredentials());
-    builder.RegisterService(&service);
-    server = builder.BuildAndStart();
-    runningThread = std::thread([this] { server->Wait(); });
-  }
-
-  void Shutdown() {
-    if (server)
-      server->Shutdown();
-    runningThread.join();
-  }
-
-private:
-  ExternalRpcService service;
-  std::unique_ptr<grpc::Server> server;
-  std::thread runningThread;
-};
-
-}
 
 #endif //PPCA_RAFT_EXTERNAL_RPC_SERVICE_H
